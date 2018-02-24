431950bb00e2a4732a20e2f1769b519684f264b4
RinGoku <yuyu80105@gmail.com>
2018-02-24 14:55:05 +0900

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+// 追加！
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatDatepickerModule, MatNativeDateModule, MatTabsModule, MatTableModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // 追加！
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTabsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/timecard/timecard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild, ElementRef, forwardRef, AfterViewInit } from '@angular/core';
+import { DateAdapter, NativeDateAdapter } from '@angular/material';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-tv-schedule',
+    templateUrl: './timecard.component.html',
+    styleUrls: ['./timecard.component.scss'],
+    providers: [
+    ]
+})
+export class TimecardComponent implements OnInit {
+    selectedDate
+    constructor(dateAdapter: DateAdapter<NativeDateAdapter>, private router: Router) {
+        dateAdapter.setLocale('jp');
+    }
+    ngOnInit() {
+        this.selectedDate = new Date();
+    }
+    getChangedValue($event): void {
+        this.selectedDate = $event;
+    }
+}
